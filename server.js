cf76bfa493600454e119379cc36bb24c4153c7b4
Frankely Diaz <diazfrankely@gmail.com>
2015-11-07 10:23:48 -0500

    Event

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ var app = express();
 var port = process.env.PORT || 1337;
 
 var events = [];
+var count = 0;
 
 app.get('/',function(req, res) {
   res.send(events);
+});
+
+app.get('/count',function(req, res) {
+  count = count + 1;
+  res.send(count);
 });
 
 app.post('/notifyEvent', function(req, res) {
