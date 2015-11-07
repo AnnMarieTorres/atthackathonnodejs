fd997abe6ac83754240bafd8297ef2bd5f6a8ce7
Frankely Diaz <diazfrankely@gmail.com>
2015-11-07 12:05:44 -0500

    A

--- server.js
+++ server.js
@@ -9,14 +9,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 var events = [];
+var calls = [];
 
 app.get('/events',function(req, res) {
+  res.json(events);
+});
+
+app.get('/calls',function(req, res) {
   res.json(events);
 });
 
 app.post('/notifyEvent', function(req, res) {
   events.push(req.body.eventNotification);
   res.send(events);
+});
+
+app.post('/collectEvent', function(req,res) {
+/*
+​{"eventNotification": {
+     "callSessionIdentifier": "",
+     "calledParticipant": "",
+     "callingParticipant": "",
+     "callingParticipantName": "",
+     "eventDescription": {"mediaResponse": "1"},
+     "rel": "PlayAndCollectInteractionSubscription",
+     "notificationType": "PlayAndCollect",
+     "reference": "1234-abcd_CS1",
+     "responseFrom": "sip:+15552226806@foundry.att.com"
+}}
+*/
+  var selectedOption = req.body.eventNotification.eventDescription.mediaResponse;
+  var phone = req.body.eventNotification.responseFrom;
+
+  calls.push({
+    selectedOption: selectedOption,
+    phone: phone
+  });
+
 });
 
 
