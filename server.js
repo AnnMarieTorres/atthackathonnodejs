2f99d1bcc2ffd45420b5f59ac07f3e66fb138fbf
Frankely Diaz <diazfrankely@gmail.com>
2015-11-07 11:18:18 -0500

    ASDD

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var http = require('http')
 var express = require('express');
+
 var app = express();
 var port = process.env.PORT || 1337;
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/',function(req, res) {
   res.send('events');
@@ -12,8 +18,10 @@ app.get('/count',function(req, res) {
 });
 
 app.post('/notifyEvent', function(req, res) {
-  res.send('OK');
+  console.log(req.body);
+  res.json(req.body);
 });
+
 
 
 var server = app.listen(port, function () {
