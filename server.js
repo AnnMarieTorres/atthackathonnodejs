6bc617ce2bd3a78205fd7ef4ab84b0b5035a40d6
Frankely Diaz <diazfrankely@gmail.com>
2015-11-07 10:13:36 -0500

    Event

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var http = require('http')
+var express = require('express');
+var app = express();
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+
+var events = [];
+
+app.get('/',function(req, res) {
+  res.send(events);
+});
+
+app.post('/notifyEvent', function(req, res) {
+  events.push(req);
+  res.send('OK');
+});
+
+
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
